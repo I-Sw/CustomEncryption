e0da3f562ffd75ccaf99ef6d96173f9c6caa0644
I-Sw <50085577+I-Sw@users.noreply.github.com>
2020-09-22 15:25:59 +0100

    Return to this program.
    I've returned to this program, experimenting with it to ensure it encrypts and decrypts correctly, while brainstorming new potential options for use.

--- src/Controller.java
+++ src/Controller.java
@@ -1,7 +1,7 @@
 public class Controller {
     public static void main(String args[])
     {
-        String encryptedText = Encrypt.encrypt("Finally got this hellish program working?");
+        String encryptedText = Encrypt.encrypt("Text to decrypt goes here!");
         System.out.println(encryptedText);
 
         System.out.println(Decrypt.decrypt(encryptedText));
